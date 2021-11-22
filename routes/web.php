4ca34c3826e5ca2b928812ae36af385ae7a7c376
MartinNoboa <nomonoboa@gmail.com>
2021-11-21 22:02:24 -0600

    Agregar Fortify

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,3 +21,5 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+
